c1058abb112452a022b9b22576447fdc6f7d01cd
Cecília Lima Coelho <cececilia121613@gmail.com>
2024-03-09 15:11:20 -0300

    changes

--- Semana_1/Exercicio_peixinho/peixe.js
+++ Semana_1/Exercicio_peixinho/peixe.js
@@ -3,6 +3,12 @@ var config = {
     width: 800,
     height: 600,
 
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+     },
+ 
+
     scene: {
         preload: preload,
         create: create,
@@ -15,7 +21,7 @@ var game = new Phaser.Game(config);
 var peixinho;
 
 function preload () {
-    this.load.image('inteli', "assets/inteli.png");
+    this.load.image('marclaro', "assets/marclaro.png");
 
     //carregar o peixe
     this.load.image('peixe', 'assets/peixes/peixe1.png');
@@ -26,22 +32,44 @@ function preload () {
     //carregar família
     this.load.image('fml', 'assets/fml.png');
 
+    this.load.image('mar', 'assets/marescuro.png');
+
+    this.load.image('tartaruga', 'assets/tartaruga.png')
+
 
 }
 
 function create () {
 
-    this.add.image(400, 300, 'inteli');
+
+    if(game.scale.orientation === Phaser.Scale.LANDSCAPE){
+        this.add.image(400, 300, 'mar');
+    } else if(game.scale.orientation === Phaser.Scale.PORTRAIT){
+        this.add.image(400, 300, 'marclaro');
+    }
 
 
-    //adcionar o peixe na tela
-    peixinho = this.add.image(400, 300, 'peixe').setOrigin(0.5, 0.5).setFlip(true, false);
+    if (game.device.os.desktop){
+        peixinho = this.add.image(400, 300, 'tartaruga').setOrigin(0.5, 0.5).setFlip(true, false);
+    } else{
+        
+        peixinho = this.add.image(400, 300, 'peixe').setOrigin(0.5, 0.5).setFlip(true, false);
+    }
 
     //add família
     this.add.image(400, 525, 'fml').setScale(0.7).setOrigin(0.5, 0.9);
 
     //adcionar o logo na tela
     this.add.image(400, 525, 'logo').setScale(0.5);
+
+    game.scale.on('orientationchange', function(orientation) {
+        if (orientation === Phaser.Scale.PORTRAIT) {
+            console.log('PORTRAIT')
+        }  
+        if (orientation === Phaser.Scale.LANDSCAPE) {
+            console.log('LANDSCAPE')
+        } 
+    });
 }
 
 function update() {
